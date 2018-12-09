31cf178c4c18ced64abc2c4101e7580ce2b0b333
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2018-12-09 19:50:40 +0300

    add photo orientations

---
+++ unsplash/orientation.go
@@ -0,0 +1,10 @@
+package unsplash
+
+// Orientation is photo orientation
+type Orientation string
+
+const (
+	OrientationLandscape Orientation = "landscape"
+	OrientationPortrait  Orientation = "portrait"
+	OrientationSquarish  Orientation = "squarish"
+)
