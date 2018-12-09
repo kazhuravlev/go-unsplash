3eb1be5380ba825ad983a28c36742c26e830a20e
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2018-12-10 01:24:06 +0300

    add parser for rate limit headers

--- unsplash/unsplash.go
+++ unsplash/unsplash.go
@@ -3,12 +3,14 @@ package unsplash
 import (
 	"errors"
 	"net/http"
+	"strconv"
 )
 
 var (
-	ErrUnexpected   = errors.New("unexpected error")
-	ErrBadRequest   = errors.New("bad request")
-	ErrUnauthorized = errors.New("unauthorized")
+	ErrUnexpected    = errors.New("unexpected error")
+	ErrInvalidLimits = errors.New("invalid limits")
+	ErrBadRequest    = errors.New("bad request")
+	ErrUnauthorized  = errors.New("unauthorized")
 )
 
 const (
@@ -32,4 +34,36 @@ func handleError(resp *http.Response) error {
 	default:
 		return ErrUnexpected
 	}
+}
+
+type RateLimit struct {
+	Limit     int
+	Remaining int
+}
+
+func getLimits(resp *http.Response) (*RateLimit, error) {
+	limit := resp.Header.Get(rateLimitHeaderTotal)
+	if limit == "" {
+		return nil, ErrInvalidLimits
+	}
+
+	remaining := resp.Header.Get(rateLimitHeaderRemaining)
+	if remaining == "" {
+		return nil, ErrInvalidLimits
+	}
+
+	limitInt, err := strconv.ParseInt(limit, 10, 64)
+	if err != nil {
+		return nil, ErrInvalidLimits
+	}
+
+	remainingInt, err := strconv.ParseInt(remaining, 10, 64)
+	if err != nil {
+		return nil, ErrInvalidLimits
+	}
+
+	return &RateLimit{
+		Limit:     int(limitInt),
+		Remaining: int(remainingInt),
+	}, nil
 }
